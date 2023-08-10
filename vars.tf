8698a330fbfc5473991e63aa60f06902d1398bc6
shaphill <shaphill@cisco.com>
2023-08-10 16:45:37 -0400

    update vars.tf

--- vars.tf
+++ vars.tf
@@ -30,28 +30,6 @@ variable "contract" {
   type = map(string)
 }
 
-variable "l3out" {
-  type = map(string)
-}
-
-variable "ext_subs" {
-  type = set(string)
-}
-
-variable "nodes" {
-  type = map(object({
-    name   = string
-    rtr_id = string
-  }))
-}
-
-variable "vpc_members" {
-  type = map(object({
-    side = string
-    addr = string
-  }))
-}
-
 variable "static_path" {
   type = map(string)
 }
